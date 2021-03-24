91c45c90d0a42da337d63aea1d44264f6d7018ed
Jamie Brandon <jamie@scattered-thoughts.net>
2021-03-24 15:15:40 -0700

    Put all deps in an overlay

--- kafka-streams/shell.nix
+++ kafka-streams/shell.nix
@@ -2,25 +2,13 @@ let
   
   pkgs = import ../pkgs.nix;
   
-  apacheKafka = pkgs.apacheKafka.overrideAttrs (oldAttrs: rec {
-      version = "2.7.0";
-      src = pkgs.fetchurl {
-          url = "mirror://apache/kafka/${version}/kafka_2.13-${version}.tgz";
-          sha256 = "143zrghrq40lrwacwyzqkyzg4asax7kxg9cgnkn2z83n6rv4pn0x";
-      };
-      installPhase = ''
-        mkdir -p $out/bin
-        cp bin/zookeeper* $out/bin
-      '' + oldAttrs.installPhase;
-  });
-  
 in 
 
 pkgs.mkShell {
     buildInputs = [
-        apacheKafka
         pkgs.maven
         pkgs.jre8
+        pkgs.apacheKafka
         
         pkgs.which
     ];
--- pkgs.nix
+++ pkgs.nix
@@ -1,5 +1,35 @@
+# pin package index to nixos-20.09
+# TODO will this fail with patch releases?
 import (builtins.fetchTarball {
     name = "nixos-20.09";
     url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
     sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
-  }) {}
+}) {
+    overlays = [(self: super: {
+    
+        # override kafka to v2.7.0
+        apacheKafka = super.apacheKafka.overrideAttrs (oldAttrs: rec {
+            version = "2.7.0";
+            src = self.fetchurl {
+                url = "mirror://apache/kafka/${version}/kafka_2.13-${version}.tgz";
+                sha256 = "143zrghrq40lrwacwyzqkyzg4asax7kxg9cgnkn2z83n6rv4pn0x";
+            };
+            # install zookeeper scripts
+            installPhase = ''
+                mkdir -p $out/bin
+                cp bin/zookeeper* $out/bin
+            '' + oldAttrs.installPhase;
+        });
+        
+        # override flink to v1.12.2
+        flink = super.flink.overrideAttrs (oldAttrs: rec {
+            flinkVersion = "1.12.2";
+            name = "flink-${flinkVersion}";
+            src = self.fetchurl {
+                url = "mirror://apache/flink/${name}/${name}-bin-scala_2.11.tgz";
+                sha256 = "17c2v185m3q58dcwvpyzgaymf7767m8dap0xb318ijphb9sapvpk";
+            };
+        });
+        
+    })];
+}
