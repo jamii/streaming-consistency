a49736039da48759378101843d01f96c9a1391df
Jamie Brandon <jamie@scattered-thoughts.net>
2021-03-30 12:36:33 -0700

    Dont print output keys

--- flink/run.sh
+++ flink/run.sh
@@ -110,7 +110,6 @@ kafka-console-consumer.sh \
     --topic outputs \
     --from-beginning \
     --formatter kafka.tools.DefaultMessageFormatter \
-    --property print.key=true \
     --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
     --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
     > ./tmp/outputs &
