fad73a80156d8bb979b75c1b4ac31f74d2a13d1a
Jamie Brandon <jamie@scattered-thoughts.net>
2021-04-07 20:04:27 -0700

    Working datastream example

--- flink-datastream/src/main/java/JsonDeserializationSchema.java
+++ flink-datastream/src/main/java/JsonDeserializationSchema.java
@@ -3,27 +3,28 @@ package net.scattered_thoughts.streaming_consistency;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.IOException;
 
-public class TransactionDeserializationSchema implements DeserializationSchema<Transaction> {
+public class JsonDeserializationSchema implements DeserializationSchema<JsonNode> {
 
     private static final long serialVersionUID = 1L;
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public Transaction deserialize(byte[] message) throws IOException {
-        return objectMapper.readValue(message, Transaction.class);
+    public JsonNode deserialize(byte[] message) throws IOException {
+        return objectMapper.readTree(message);
     }
 
     @Override
-    public boolean isEndOfStream(Transaction nextElement) {
+    public boolean isEndOfStream(JsonNode nextElement) {
         return false;
     }
 
     @Override
-    public TypeInformation<Transaction> getProducedType() {
-        return TypeInformation.of(Transaction.class);
+    public TypeInformation<JsonNode> getProducedType() {
+        return TypeInformation.of(JsonNode.class);
     }
 }
