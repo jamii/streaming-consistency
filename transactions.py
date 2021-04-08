ab2730b10f163e87d6bd08b2b654ff10beeb0b55
Jamie Brandon <jamie@scattered-thoughts.net>
2021-04-07 23:45:38 -0700

    Better distribution of commit timestamps

--- transactions.py
+++ transactions.py
@@ -5,8 +5,10 @@ import json
 
 random.seed(42)
 max_id = 1000000
+transactions = []
 for id in range(0,max_id):
-    second = ((50 * id) // max_id) + random.randint(0,10)
+    second = ((60 * id) // max_id)
+    delay = random.randint(0,10)
     row = json.dumps({
         'id': id,
         'from_account': random.randint(0,9),
@@ -14,4 +16,7 @@ for id in range(0,max_id):
         'amount': 1,
         'ts': f'2021-01-01 00:00:{second:02d}.000',
     })
+    transactions.append((second + delay, id, row))
+transactions.sort()
+for (_, id, row) in transactions:
     print(f'{id}|{row}')
