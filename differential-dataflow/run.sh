53c428b73d8e469ce4c91683941d02e834f427da
Jamie Brandon <jamie@scattered-thoughts.net>
2021-04-16 20:25:51 -0700

    Make tmp dir

--- differential-dataflow/run.sh
+++ differential-dataflow/run.sh
@@ -9,6 +9,7 @@ THIS_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 DATA_DIR=$THIS_DIR/tmp
 echo "Data will be stored in $DATA_DIR"
 rm -rf $DATA_DIR/*
+mkdir -p $DATA_DIR/{config,logs}
 
 echo "Feeding inputs"
 $DATAGEN | cut -d'|' -f2 > $DATA_DIR/transactions
